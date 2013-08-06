40fae2b75b5acda2322d67074315553197df4e47
Matthias Steiner <matthias.steiner@inscope.net>
2013-08-06 10:19:23 +0200

    Initial Setup (SAP TechEd 2013 CD265 excercise)
    Change-Id: I3db683cd260e81f1f7a6b7dc2f3a55e910a0f374
    Signed-off-by: Matthias Steiner <matthias.steiner@inscope.net>

--- teched-ui-reviews-web/src/main/webapp/espm-ui-reviews-web/main.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.controller("espm-ui-reviews-web.main", {
-
-	openSettingsDialog : function() {
-		var oSettingsView = sap.app.viewCache.get("settings");
-		oSettingsView.getController().openDialog();
-	}
-});
